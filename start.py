5c9858a93cd0919ad14bcac9f89e52b791b73f0d
Aashray Jain <aashray.jain@oracle.com>
2024-03-05 18:00:05 +0530

    MultiProcessing OLD WAY : Looping

--- start.py
+++ start.py
@@ -5,19 +5,29 @@ import time
 start = time.perf_counter()
 
 
-def do_something():
-    print("Sleeping")
-    time.sleep(1)
+def do_something(seconds):
+    print(f"Sleeping for : {seconds} second(s)")
+    time.sleep(seconds)
     print("Done")
 
-p1 = multiprocessing.Process(target=do_something)
-p2 = multiprocessing.Process(target=do_something)
+# p1 = multiprocessing.Process(target=do_something)
+# p2 = multiprocessing.Process(target=do_something)
 
-p1.start()
-p2.start()
+# p1.start()
+# p2.start()
 
-p1.join()
-p2.join()
+# p1.join()
+# p2.join()
+
+processes = []
+
+for _ in range(10):
+    p = multiprocessing.Process(target=do_something, args=[1.5])
+    p.start()
+    processes.append(p)
+
+for process in processes:
+    process.join()
 
 end = time.perf_counter()
 
